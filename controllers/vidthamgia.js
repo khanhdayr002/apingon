2ec0e2dd2506d57e261d6df6d1ffd5c411823ce5
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-08-27 21:49:56 +0700

    Update vidthamgia.js

--- controllers/vidthamgia.js
+++ controllers/vidthamgia.js
@@ -1,7 +1,7 @@
 const { errorHandler } = require("../utils");
 
-exports.vidthamgia = async (req, res, next) => {
-  var data = require('fs-extra').readFileSync(__dirname + '/../data/vidthamgia.txt', 'utf-8').split('\n');
+exports.videogai = async (req, res, next) => {
+  var data = require('fs-extra').readFileSync(__dirname + '/../data/videogai.json', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-res.json({"Authors":"hoangphuong","data":`${link}`});
+res.json({"Authors":"Gkhanh","url":`${link}`});
 };
