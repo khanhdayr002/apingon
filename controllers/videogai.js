596c4692e9b43920d38ac4b84dbdadd06e3acf30
khanhdayr002 <154343426+khanhdayr002@users.noreply.github.com>
2024-08-27 21:55:31 +0700

    Update videogai.js

--- controllers/videogai.js
+++ controllers/videogai.js
@@ -3,5 +3,5 @@ const { errorHandler } = require("../utils");
 exports.videogai = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../data/videogai.json', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
-res.json({"Authors":"Gkhanh","data":`${link}`});
+res.json({"Authors":"Gkhanh","url":`${link}`});
 };
